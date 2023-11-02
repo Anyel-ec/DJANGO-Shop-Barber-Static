2901864d355ca45f496bc33e1cd7716375a00deb
JerickTwO <137414207+JerickTwO@users.noreply.github.com>
2023-11-02 14:14:56 -0500

    second_commit

--- appbarberia/models.py
+++ appbarberia/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# agregar modelos 
+class Customer(models.Model):
+    dni = models.CharField(max_length=10)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone=models.CharField(max_length=10, blank=True,null=True)
